a873362e70a4b14a8fdc65d4848d28c2a9207c2f
GM7Avila <guilherme.medeiros.avila@gmail.com>
2025-02-21 22:10:29 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
+import chess.ChessMatch;
+
 
 public class UI {
 
@@ -55,6 +57,13 @@ public class UI {
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
